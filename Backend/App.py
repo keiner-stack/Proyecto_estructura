3fc78660d0e7839d9f07f60723b9a71b4d16fb77
KeinerBv <keinerporras124@unisangil.edu.co>
2025-05-21 13:21:34 -0500

    Actualizacion de 21/05/2025

--- Backend/App.py
+++ Backend/App.py
@@ -1,13 +1,1 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-mascotas = [
-    {"id": 1, "nombre": "Firulais", "dueño": "Juan"}
-]
-
-@app.route('/mascotas')
-def get_mascotas():
-    return jsonify(mascotas)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#hola
